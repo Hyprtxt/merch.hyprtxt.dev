36a9d52d4f7f7d29774e68fe73a585606eacc8b1
Taylor Y <tdy721@gmail.com>
2023-02-16 16:12:16 -0700

    test

---
+++ test/1_puppet_test.js
@@ -0,0 +1,15 @@
+import { assertEquals } from "$std/testing/asserts.ts";
+import { freshPuppetTestWrapper } from "@/test/runner.js";
+import { BASE_URL } from "@/utils/config.js";
+
+Deno.test(
+  "Public Pages Testing",
+  freshPuppetTestWrapper(async (t, page) => {
+    await t.step("The homepage should work", async () => {
+      const response = await page.goto(`${BASE_URL}`, {
+        waitUntil: "networkidle2",
+      });
+      assertEquals(response.status(), 200);
+    });
+  }),
+);
